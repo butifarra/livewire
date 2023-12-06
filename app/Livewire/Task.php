045f94685f4069d71108b66cb51c95a2e2d1b6d3
Denis Pujadas <elbutifarra@gmail.com>
2023-12-05 23:51:23 -0300

    5

--- app/Livewire/Task.php
+++ app/Livewire/Task.php
@@ -26,6 +26,21 @@ class Task extends Component
         /* $this->validate(['tarea' => 'required|max:20']); */
         $this->validate(['tarea' => 'max:20']);
     }
+
+    public function edit(TaskModel $task)
+    {
+        $this->task = $task; //asingo la task recibida a la task de mi clase que instancié en mount
+        //Al asignar la propiedad automáticamente aparecen los datos en la caja ded texto porque ya está
+        //enlazada. Como yo no usé el modelo del curso, agrego esto para que eso pase:
+        $this->tarea = $task->text;
+    }
+
+    public function done(TaskModel $task)
+    {
+        $task->update(['done' => !$task->done]); //Acá pone lo contrario de lo que tiene, porque es boolean
+        $this->mount(); //Hago mount porque no me molesta que reinicie la $task
+    }
+
     public function save()
     {
         /*    dd($this->task); */
@@ -39,7 +54,20 @@ class Task extends Component
         $this->dispatch('taskSaved', 'Tarea guardada'); //Envía el evento al componente padre, y el padre lo escucha con protected $listeners
 
     }
+
+    public function delete($id)
+    {
+        $task1 = TaskModel::find($id);
+        //También podría ser: $task1 = TaskModel::find($id)->delete;
+        if (!is_null($task1)) {
+            $task1->delete();
+        }
+        $this->dispatch('taskSaved', 'Tarea eliminada');
+        $this->mount();
+    }
+
     public function render()
+
     {
         return view('livewire.task');
     }
